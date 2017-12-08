fe501da08cbcdeee6eeb0c51fefbb5b64a7498dd
AramLim <arami7@naver.com>
2017-12-08 18:00:50 +0900

    Add UART code

---
+++ Development_3_Team/source/UART_Arduino/UART.ino
@@ -0,0 +1,26 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   if (Serial1.available())
+  {
+     Serial.write(Serial1.read());
+  }
+
+}
+  
+/*
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(100);
+*/
+  /*
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+  */
--- Development_3_Team/source/UART_Atmega128/UART.c
+++ Development_3_Team/source/UART_Atmega128/UART.c
@@ -3,21 +3,18 @@
 #include <delay.h>
 
 void Putch(char data);
-void TX0_CH(unsigned char data);
-void TX0_STR(unsigned char *str);
 
 void main(void)
 {
-    unsigned char string[] = "Hello World! \r\n";
+    unsigned char string[] = "Hello World! \r\n";     
     unsigned char i = 0;
-    char *pStr;
     
-    DDRE = 0xfe;
-    UCSR0A = 0x0;
-    UCSR0B = 0b00011000;
-    UCSR0C = 0b00000110;
+    DDRE = 0xfe;                 // PE 0, 1 -> RX, TX Pin
+    UCSR0A = 0x0;                
+    UCSR0B = 0b00011000;         // Enable TXENn, RXENn Enable
+    UCSR0C = 0b00000110;         // No Parity bit, 1 Stop bit, Transmit 8 bit data 
     UBRR0H = 0;
-    UBRR0L = 8;      //115200 bps
+    UBRR0L = 8;      // 115200 bps
          
         
     while(1){               
@@ -26,8 +23,6 @@ void main(void)
         
         delay_ms(500);
         i = 0;
-        //TX0_CH('a');
-        //delay_ms(1000);
     }
     
 }                                        
@@ -35,17 +30,7 @@ void main(void)
 void Putch(char data){
     while((UCSR0A & 0x20) == 0x0);
     UDR0 = data;          
-    //UCSR0A |= 0x02;
 }
 
-void TX0_CH(unsigned char data){
-    while(!(UCSR0A & (1 << UDRE0)));
-    
-    UDR0 = data;
-}
 
-void TX0_STR(unsigned char *str){
-    while(*str){
-        TX0_CH(*str++);
-    }
-}
+
